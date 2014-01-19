51f2990f180ff53e0ba5708b3962ad1dc0fa378d
Gregor Peach <gregorpeach@gmail.com>
2014-01-18 23:16:31 -0800

    Added graphical main+pakage!

---
+++ src/codedat/main/Graphics.java
@@ -0,0 +1,28 @@
+package codedat.main;
+import janus.engine.SimpleGraphics;
+import janus.engine.pens.SimplestPen;
+public class Graphics extends SimpleGraphics{
+
+	protected Graphics() {
+		super(800, 600, "RPG");
+	}
+
+	@Override
+	public void draw(SimplestPen pen) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(SimplestPen pen) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(SimplestPen pen) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/codedat/main/Main.java
@@ -0,0 +1,7 @@
+package codedat.main;
+
+public class Main {
+	public static void main(String[] args){
+		
+	}
+}
