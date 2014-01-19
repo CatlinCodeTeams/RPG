ccb9f9a05fbd0b61c12dea17598391ea2066fe47
riddlet <riddlet@STU16-03.catlin.edu>
2014-01-18 23:33:15 -0800

    Drawer stuff

--- src/codedat/main/Drawer.java
+++ src/codedat/main/Drawer.java
@@ -1,7 +1,18 @@
 package codedat.main;
 
+import java.awt.image.BufferedImage;
+
+import codeday.rpg.interfaces.Square;
+
 public class Drawer {
 
+
+	public void draw(){
+		for (Square q: Main.square_list){
+			BufferedImage image = q.getMyImage();
+			Main.pen.simplePen.drawImage(image, q.getX(), q.getY(), 40, 40, null);
+		}
 	
+	}
 	
 }
