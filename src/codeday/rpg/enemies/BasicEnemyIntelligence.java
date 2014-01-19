ed7ad8f2fbe2417f560f0f0b9a6f38a8981ae475
Jackson <Jackson@10.16.120.56>
2014-01-19 00:36:04 -0800

    asd

--- src/codeday/rpg/enemies/BasicEnemyIntelligence.java
+++ src/codeday/rpg/enemies/BasicEnemyIntelligence.java
@@ -24,48 +24,27 @@ public abstract class BasicEnemyIntelligence implements Enemy {
 	}
 
 	@Override
-<<<<<<< HEAD
-	public void takeDamage(int damage) {
-		this.health -= damage;
-
-=======
 	public boolean takeDamage(int damage) {
 		health -= damage;
 		if (health <= 0) {
 			return true;
 		}
 		return false;
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 	}
 
 	@Override
 	public int getSpeed() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-		return this.speed;
-=======
 		return speed;
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 	}
 
 	@Override
 	public int getX() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-		return this.x;
-=======
 		return x;
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 	}
 
 	@Override
 	public int getY() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-		return this.y;
-=======
 		return y;
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 	}
 
 	@Override
@@ -75,10 +54,6 @@ public abstract class BasicEnemyIntelligence implements Enemy {
 
 	@Override
 	public boolean drawBackground() {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-=======
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 		return true;
 	}
 
