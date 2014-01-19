e1162a79fbfc3e76ee55fe0f08c0f968d586ef36
Gregor Peach <gregorpeach@gmail.com>
2014-01-19 06:22:04 -0800

    Fixed merge.

--- src/codeday/main/Graphics.java
+++ src/codeday/main/Graphics.java
@@ -45,16 +45,7 @@ public class Graphics extends SimpleGraphics{
 	@Override
 	public void start(SimplestPen pen) {
 		img=imgs;
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
 		ArrayList<Square> s=new ArrayList<Square>();
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
-		square_array= new Square[40][30];
-
-=======
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 		loadImage("wall.png");
 		loadImage("spr_main.png");
 		loadImage("spr_rat_down2.png");
@@ -76,11 +67,7 @@ public class Graphics extends SimpleGraphics{
 				ls.add(new ArrayList<Square>());
 				for(char c:line.toCharArray()){
 					two++;
-<<<<<<< HEAD
-					if(c=='w'){
-=======
 					if(c=='w'){                                               //Random blocks were dissapearing when called to draw, swithcing one and two seems to fix it.
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 						ls.get(ls.size()-1).add(new Wall(this.imgs.get("wall.png"),two*40,one*40));
 					}
 					else if(c==' '){
