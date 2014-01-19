912ff5d93602108dab5af7042d737be8cbe21898
Gregor Peach <gregorpeach@gmail.com>
2014-01-19 02:53:09 -0800

    test

--- src/codeday/main/Graphics.java
+++ src/codeday/main/Graphics.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -38,29 +42,57 @@ public class Graphics extends SimpleGraphics{
 	@Override
 	public void start(SimplestPen pen) {
 		img=imgs;
-<<<<<<< HEAD
-<<<<<<< HEAD
 		ArrayList<Square> s=new ArrayList<Square>();
-		BufferedInputStream stream=new BufferedInputStream(FileLoader.class.getResourceAsStream("map1.txt"));
 		square_array= new Square[40][30];
-=======
-=======
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
-		
->>>>>>> branch 'master' of https://github.com/CodeDayPortlandRPG/RPG.git
 		loadImage("wall.png");
 		loadImage("spr_main.png");
 		loadImage("spr_rat_down2.png");
 		loadImage("spr_sprite.png");
-		
-		square_array = new Square[40][30];
-		
-		for (int k=0; k<10; k++){
-			square_array[5+k][10] =new Wall(this.imgs.get("wall.png"), 200+k*40,400);
+		ArrayList<ArrayList<Square>> ls=new ArrayList<ArrayList<Square>>();
+		BufferedReader reader=null;
+		try {
+			reader = new BufferedReader(new FileReader(new File(FileLoader.class.getResource("map1.txt").toURI())));
+		} catch (FileNotFoundException | URISyntaxException e) {
+			e.printStackTrace();
 		}
-		RealPlayer player = new RealPlayer(this.imgs.get("spr_main.png"), 120,120);
-		square_array[3][3] = player;
-		
+		String line;
+		try {
+			int one=-1;
+			while ((line = reader.readLine()) != null) {
+				one++;
+				int two=-1;
+				ls.add(new ArrayList<Square>());
+				for(char c:line.toCharArray()){
+					two++;
+					if(c=='w'){
+						ls.get(ls.size()-1).add(new Wall(this.imgs.get("wall.png"),one*40,two*40));
+					}
+					else{
+						ls.get(ls.size()-1).add(null);
+					}
+				}
+			}
+			line=null;
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		square_array = new Square[ls.get(0).size()][ls.size()];
+		System.out.println(ls.size());
+		System.out.println(ls.get(0).size());
+		for(int x=0;x<ls.get(0).size();x++){
+			for(int y=0;y<ls.size();y++){
+				square_array[x][y]=ls.get(y).get(x);
+			}
+		}
+//		
+//		
+//		for (int k=0; k<10; k++){
+//			square_array[5+k][10] =new Wall(this.imgs.get("wall.png"), 200+k*40,400);
+//		}
+//		RealPlayer player = new RealPlayer(this.imgs.get("spr_main.png"), 120,120);
+//		square_array[3][3] = player;
+//		
 		this.endProgramOnClose=true;
 		pen.setBackground(Color.WHITE);
 		turner=new TurnManager(null,null);
